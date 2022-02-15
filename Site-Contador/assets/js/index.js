fc8c1ab0199c9ce8a30d9445f687908d24e0c7e7
Nikolas Oliveira <nikolas_081@hotmail.com>
2022-02-15 13:37:48 -0300

    Site contador personalizado com Js

---
+++ Site-Contador/assets/js/index.js
@@ -0,0 +1,42 @@
+function contar(){
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
+
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar'
+    }
+    else {
+       res.innerHTML = 'Contando: <br>'
+       let i =Number(inicio.value)
+       let f =Number(fim.value)
+       let p =Number(passo.value)
+
+       if (p <= 0){
+           window.alert('Passo inválido, considerando PASSO = 1')
+           p = 1
+           
+       }
+
+       if(i<f){
+           //contagem crescente
+            for (let c= i; c<= f; c += p){
+                res.innerHTML += ` ${c} \u{1F47D}`
+            }
+            
+       }
+       else{
+           //contagem regressiva
+           for(let c = i; c>=f; c -= p){
+               res.innerHTML += `${c} \u{1F47D}` 
+           }
+           
+           
+       }
+       res.innerHTML += `\u{1F47E}`
+       
+    }
+}
